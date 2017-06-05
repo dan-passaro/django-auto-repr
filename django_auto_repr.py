347aaa958a0a9ef6451326a309818679963703bd
Dan Passaro <danpassaro@gmail.com>
2017-06-05 12:55:53 -0400

    Make Py2 compatible

--- django_auto_repr.py
+++ django_auto_repr.py
@@ -1,26 +1,24 @@
 from django.db import models
 
 
-class AutoRepr(models.Model):
+class AutoRepr(object):
     """A model mixin that generates a useful __repr__"""
 
     def _repr_format_field(self, field):
-        """Get a "bar='bar_value' str.
+        """Get a "bar='bar_value'" str.
 
-        field is a Django Field
+        field is a Django Field object.
         """
         if isinstance(field, models.ForeignKey):
             field_name = field.name + '_id'
         else:
             field_name = field.name
-        return f'{field_name}={getattr(self, field_name)!r}'
+        return "{}={!r}".format(field_name, getattr(self, field_name))
 
     def __repr__(self):
         cls = type(self)
         fields = cls._meta.fields
         parts = map(self._repr_format_field, fields)
         attrs = ', '.join(parts)
-        return f'{cls.__name__}({attrs})'
+        return '{}({})'.format(cls.__name__, attrs)
 
-    class Meta:
-        abstract = True
